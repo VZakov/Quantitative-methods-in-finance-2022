51b1ed3bd256cfecfaf8fb401deabe716799f138
VZakov <vzakov@gmail.com>
2022-03-26 16:34:02 +0200

    Homework20220324

---
+++ Homework20220324.R
@@ -0,0 +1,37 @@
+#####Problem 1#####
+# Write a function, which uses a loop to calculate factorial.
+# The base R function is called factorial and you should replicate its result.
+# This is a function, which takes two numbers, multiplies them and returns
+# the result as output:
+# MultiplicationFunction <- function(inputNumber1, inputNumber2){
+#   Result <- inputNumber1 * inputNumber2
+#   return(Result)
+# }
+# MultiplicationFunction(5, 3)
+# 
+# Write a factorial function:
+
+# FactorialFunction <- function(inputNumber){
+#   ???
+#     return(Result)
+# }
+#####Problem 1#####
+
+#####Problem 2#####
+
+#####Problem 2#####
+
+#####Problem 3#####
+# Read everything from https://r4ds.had.co.nz/transform.html, 
+# in particular chapters 5.6/5.7
+
+#Do all the exercises:
+# 5.6.7 Exercises 
+# 5.7.1 Exercises
+#####Problem 3#####
+
+#####Problem 4#####
+
+#I will add problem 2/4 and the recording later. Also upload your homeworks on
+#your own github repo.
+#####Problem 4#####
