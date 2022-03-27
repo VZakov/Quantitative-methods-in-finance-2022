761891bf1a14e7a21aa1a66693697e7e177ff24a
VZakov <49568270+VZakov@users.noreply.github.com>
2022-03-27 16:37:31 +0300

    Update Lesson1.R

--- Homework20220324.R
+++ Homework20220324.R
@@ -18,7 +18,13 @@
 #####Problem 1#####
 
 #####Problem 2#####
-
+#Write a function which takes a vector and returns its standard deviation.
+#You should get the same results as the sd() function.
+# SDFunction <- function(inputVector){
+#   ???
+#     return(Result)
+# }
+# ??? is not Result <- sd(inputVector)
 #####Problem 2#####
 
 #####Problem 3#####
@@ -31,7 +37,16 @@
 #####Problem 3#####
 
 #####Problem 4#####
-
-#I will add problem 2/4 and the recording later. Also upload your homeworks on
-#your own github repo.
+#Find the following:
+#4.1 For each carrier what is the most common destination?
+#4.2 For each carrier what is the biggest delay?
+#4.3 Which are the three plane which have flown the most/least miles?
+#4.4 What are the first/last flights for each day in February 2013?
+#4.5 Which company flew the most miles in March 2013? Which flew the least?
+#4.6 Which month had the most delays over 60 minutes?
+#4.7 What is the average time between two consecutive flights?
+#4.8 Use the SDFunction function from exercise 2 to calculate the standard deviation
+#of the flight delays for each month and for each destination.
 #####Problem 4#####
+#Upload your homeworks on your own github repo.
+#Link to the seminar https://unisofiafaculty.sharepoint.com/:v:/s/AccountingFinanceandDigitalapplicationsSeminargroupI/EfR2uYarKcRFiljWMgRb9U8BL6XsygAzJv_fu7mOCQsYzQ?e=CanPG0
--- Lesson1.R
+++ Lesson1.R
@@ -1,5 +1,5 @@
 options(scipen = 999)
 
 Change1
-
+Change2
 "https://github.com/VZakov/Quantitative-methods-in-finance-2022"
