cba87f9baac7fdd5dc84e62b4f7f123888fe77c1
VZakov <49568270+VZakov@users.noreply.github.com>
2022-04-02 17:18:15 +0300

    Create Homework_20220331.R

---
+++ Homework_20220331.R
@@ -0,0 +1,8 @@
+#There won't be any additional problems for this week.
+#Please finish all the previous homeworks and read:
+#https://r4ds.had.co.nz/transform.html (again)
+#https://r4ds.had.co.nz/relational-data.html
+#https://cran.r-project.org/web/packages/tidyquant/vignettes/TQ00-introduction-to-tidyquant.html
+#More specifically:
+#https://cran.r-project.org/web/packages/tidyquant/vignettes/TQ01-core-functions-in-tidyquant.html
+#If you have any questions, feel free to contact me
