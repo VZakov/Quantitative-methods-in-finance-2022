da96e302b69bcdd1d9f300d990a28324a31586aa
VZakov <49568270+VZakov@users.noreply.github.com>
2022-05-23 11:34:25 +0300

    Add files via upload

---
+++ Homework20220519.R
@@ -0,0 +1,24 @@
+# https://dplyr.tidyverse.org/articles/dplyr.html - the basic
+# https://dplyr.tidyverse.org/articles/grouping.html - group by in details
+# https://dplyr.tidyverse.org/articles/base.html - comparing dplyr functions
+# to the base R function.
+
+#####Problem 1#####
+# Download data for a stock of your choice and do the following:
+# Calculate the 20 day SMA of the stock price and define upper and
+# lower bounds around it which are equal to SMA +-2 standard deviation
+# the past observations used to calculate the SMA.
+# Employ the following strategy and compare to a baseline strategy of buy and hold:
+# If the price goes above the upper bound - sell.
+# If the price goes below the lower bound - buy.
+#####Problem 1#####
+
+#####Problem 2#####
+# Calculate the RSI using the instruction about the formula from here:
+# https://www.investopedia.com/terms/r/rsi.asp
+# Employ the following strategy and compare to a baseline strategy of buy and hold:
+# If the RSI above 65 - sell.
+# If the price goes below 35 - buy.
+#####Problem 2#####
+
+#Upload your homeworks on your own github repo and send me an email, when you are done.
